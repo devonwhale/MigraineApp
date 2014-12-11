1637b3a039896103d389323db82a90137a01f10c
devonwhale <george@cattlebell.com>
2014-12-11 22:09:17 +0000

    Created blank activities to transition to

--- app/src/main/java/com/example/migraineapp/MainActivity.java
+++ app/src/main/java/com/example/migraineapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.migraineapp;
 
 import android.app.DialogFragment;
-import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity implements LoginPopup.LoginPopupListener {
@@ -14,6 +18,15 @@ public class MainActivity extends ActionBarActivity implements LoginPopup.LoginP
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button but_attack = (Button) findViewById(R.id.recAttack);
+        but_attack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mv_att = new Intent(getApplicationContext(), AttackActivity.class);
+                startActivity(mv_att);
+            }
+        });
     }
 
 
@@ -39,6 +52,10 @@ public class MainActivity extends ActionBarActivity implements LoginPopup.LoginP
         if (id == R.id.action_login) {
             DialogFragment d = new LoginPopup();
             //d.show(getSupportFragmentManager(), "LoginPopup");
+        }
+
+        if (id == R.id.action_help) {
+            //Display Help fragment
         }
 
         return super.onOptionsItemSelected(item);
